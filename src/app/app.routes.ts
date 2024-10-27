faaf922c7599ac0a11da5c0cb66e277547166029
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2024-10-27 16:30:51 +0530

    primeng added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,43 @@
 import { Routes } from '@angular/router';
-import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
-import { BlogSearchComponent } from './components/blog-search/blog-search.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { LoginComponent } from './auth/components/login/login.component';
-import { SignupComponent } from './auth/components/signup/signup.component';
-import { HeaderComponent } from './components/header/header.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HeaderComponent,
+    loadComponent: () => import('./components/header/header.component').then(m => m.HeaderComponent),
     children: [
       {
         path: '',
-        component: BlogSearchComponent, // Default path to BlogSearchComponent
+        loadComponent: () => import('./components/blog-search/blog-search.component').then(m => m.BlogSearchComponent), // Default path to BlogSearchComponent
       },
-      { path: 'blog/:id', component: BlogDetailComponent },
+      { 
+        path: 'blog/:id', 
+        loadComponent: () => import('./components/blog-detail/blog-detail.component').then(m => m.BlogDetailComponent) 
+      },
     ],
   },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: '**', component: PageNotFoundComponent },
+  { path: 'login', loadComponent: () => import('./auth/components/login/login.component').then(m => m.LoginComponent) },
+  { path: 'signup', loadComponent: () => import('./auth/components/signup/signup.component').then(m => m.SignupComponent) },
+  { 
+    path: 'admin',
+    loadComponent: () => import('./admin/components/admin/admin.component').then(m => m.AdminComponent),
+    children: [
+      {path : 'dashboard',
+        loadComponent: () => import('./admin/components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      },
+      {
+        path: 'create-blog',
+        loadComponent: () => import('./admin/components/create-blog/create-blog.component').then(m => m.CreateBlogComponent), // Default path to BlogSearchComponent
+      },
+      {
+        path: 'blog-list',  
+        loadComponent: () => import('./admin/components/blog-list/blog-list.component').then(m => m.BlogListComponent)
+      },
+      {
+        path :'edit-blog/:id',
+        loadComponent: () => import('./admin/components/create-blog/create-blog.component').then(m => m.CreateBlogComponent)
+      }
+    ]
+  },
+  { path: '**', loadComponent: () => import('./components/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent) },
 ];
+
